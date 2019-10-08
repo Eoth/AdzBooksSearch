c7f103eee925dcb32fd60be6dcc0e4b99ff95083
Théodore Nakote <theodorenak93@gmail.com>
2019-10-08 21:51:34 +0200

    change

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -12,39 +12,77 @@ import { MatDialog } from '@angular/material';
 export class SearchResultsComponent implements OnInit {
   searchValue: string; // variable pour la valeur saisi envoyé de la page de recherche
   books: any; // variable pour les livres récupérer
+  booksPost: any; // variable afficher les livres filtrer
+  cols: number;
+  rowHeight: any; // pour rendre responsive l'affichage
 
   // recuperer la liste des livres depuis le composant parent
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private booksService: BooksService,
-              public dialog: MatDialog) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private booksService: BooksService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     // Récupération de la valeur saisi
     this.searchValue = this.route.snapshot.paramMap.get('word');
 
     // Reqêtage pour récupérer des livres dans l'API
-    this.booksService.getBooks(this.searchValue)
-    .then((res: any) => {
-      this.books = res.items;
-    })
-    .catch(err => console.error(err));
+    this.booksService
+      .getBooks(this.searchValue)
+      .then((res: any) => {
+        this.books = res.items;
+        this.booksPost = this.books;
+      })
+      .catch(err => console.error(err));
+
+    // le nombre de livre selon la taille de l'écran
+    this.cols = window.innerWidth <= 500 ? 1 : 3;
+    this.rowHeight = window.innerWidth <= 500 ? '20%' : '35%';
+  }
+
+  // le nombre de livre selon la taille de l'écran dynamiquement
+  onResize(event) {
+    this.cols = event.target.innerWidth <= 500 ? 1 : 3;
+    this.rowHeight = event.target.innerWidth <= 500 ? '20%' : '35%';
+  }
+
+  // filtrer les livres afficher
+  applyFilter(filterValue: string) {
+    // tslint:disable-next-line: prefer-const
+    let randBook = []; // variable dans lequelle sera rajouter dynamiquement
+                       // tous les livres filter
+  // parcours des livres
+    for (const book of this.books) {
+      // verifie si le mot de filtrage est vide
+      if (filterValue !== ''.trim()) {
+        // verifie si le mot saisi se trouve dans le titre du livre parcourue
+        if (book.volumeInfo.title.toLowerCase().includes(filterValue.trim().toLowerCase())
+            || book.volumeInfo.authors[0].toLowerCase().includes(filterValue.trim().toLowerCase())) {
+          randBook.push(book); // si le mot saisi se trouve dasn cet livre, il est rajouter
+        }
+      } else {
+        randBook = this.books; // si le champs, réafficher les resultats initiale
+        break;
+      }
+    }
+    this.booksPost = randBook;
   }
 
   // La fonction pour retourner sur la page de recherche
   goBack(): void {
     this.router.navigate(['/search']);
   }
+
   // pop-up d'affichage des details des livres selectionnés
   openDialog(descr: any): void {
     const dialogRef = this.dialog.open(DetailViewComponent, {
       width: '50%',
-      data: {val: descr}
+      data: { val: descr }
     });
-
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
   }
-
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
